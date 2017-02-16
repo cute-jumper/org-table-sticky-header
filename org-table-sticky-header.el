f7392cb36fcb63b84f1514d0716abf61c831647c
Junpeng Qiu <qjpchmail@gmail.com>
2017-02-16 14:21:59 -0500

    Add README

--- org-table-sticky-header.el
+++ org-table-sticky-header.el
@@ -4,7 +4,7 @@
 
 ;; Author: Junpeng Qiu <qjpchmail@gmail.com>
 ;; Keywords: extensions
-;; Package-Requires: ((org "9.0.5"))
+;; Package-Requires: ((org "8.2.10"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -21,7 +21,52 @@
 
 ;;; Commentary:
 
-;;
+;;                      ______________________________
+
+;;                       ORG-TABLE-STIKCY-HEADER-MODE
+
+;;                               Junpeng Qiu
+;;                      ______________________________
+
+
+;; Table of Contents
+;; _________________
+
+;; 1 Overview
+;; 2 Installation
+;; 3 Demo
+
+
+;; A minor mode to show the sticky header for org-mode tables.
+
+
+;; 1 Overview
+;; ==========
+
+;;   Similar to `semantic-stickyfunc-mode', this package uses the header
+;;   line to show the table header when it is out of sight.
+
+
+;; 2 Installation
+;; ==============
+
+;;   To install manually:
+;;   ,----
+;;   | (add-to-list 'loat-path "/path/to/org-table-sticky-header.el")
+;;   `----
+
+;;   To use it, `M-x org-table-sticky-header-mode' in the org-mode buffer.
+
+;;   To enable it automatically in all org-mode buffers, use
+;;   ,----
+;;   | (add-hook 'org-mode-hook 'org-table-sticky-header-mode)
+;;   `----
+
+
+;; 3 Demo
+;; ======
+
+;;   [./screenshots/demo.gif]
 
 ;;; Code:
 
